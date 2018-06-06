c54c7ff5e2ed83769e0162a698319bdab24334e4
GuoGuiRong <3095764372@qq.com>
2018-06-06 17:20:33 +0800

    全局更新，添加ReadMe.MD备注

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# python-learn
-python学习的时候写的一些简单的demo
-- douban-top250 豆瓣电影top250电影资源爬取
+## python-learn
+###python学习的时候写的一些简单的demo：
+
+  ##### douban-top250 豆瓣电影top250电影资源爬取
+   
+  * douban250-1.py 使用requests获取网页资源
+  * douban250-2.py 使用BeautifulSoup解析html元素
+  * douban250-3.py 爬取豆瓣电影top250网站的电影名称，并写入movies.txt
+  * douban250-4.py 爬取豆瓣电影top250网站的电影名称和图片，并写入imags文件夹
---
+++ douban-top250/douban250-4.py
@@ -0,0 +1,47 @@
+﻿#!/usr/bin/env python
+# encoding=utf-8
+import requests
+import codecs
+from bs4 import BeautifulSoup
+
+DOWNLOAN_URL = 'https://movie.douban.com/top250'
+HEADER = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+}
+
+def parse_html(html):
+    soup = BeautifulSoup(html)
+    img_list_soup = soup.find('ol',attrs={'class':'grid_view'})
+    img_dict = {}
+    for img_li in img_list_soup.find_all('li'):
+        detail = img_li.find('div',attrs={'class':'pic'}).find('a')
+        img_name = detail.find('img')['alt']
+        img_url = detail.find('img')['src']
+        img_dict[img_name] = img_url;
+        
+    next_page = soup.find('span',attrs={'class': 'next'}).find('a')
+    if next_page:
+        return img_dict, DOWNLOAN_URL + next_page['href']
+    return img_dict, None
+		
+	
+def download_page(url):
+    data = requests.get(url, headers=HEADER).content
+    # 使用codecs解决乱码问题
+    data = data.decode('utf-8')
+    return data
+	
+def main():
+    url = DOWNLOAN_URL
+    while url:
+        html = download_page(url)
+        imgs, url = parse_html(html)
+        print(imgs, url)
+        for key in imgs.keys():
+            print(imgs[key])
+            with open('c://Users/GuiRunning/Desktop/python-learn/douban-top250/images/%s.jpg' % (key), 'wb') as fp:
+                data = requests.get(imgs[key], headers=HEADER).content
+                fp.write(data)
+
+if __name__ == '__main__':
+    main()
