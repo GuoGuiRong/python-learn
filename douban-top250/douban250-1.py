4647ffa5a0adaf073b1c5109131238aaf7e5bdb5
GuoGuiRong <3095764372@qq.com>
2018-06-06 15:19:14 +0800

    使用requests简单爬取页面资源

--- douban-top250/douban250-1.py
+++ douban-top250/douban250-1.py
@@ -4,7 +4,10 @@ import requests
 import codecs
 DOWNLOAN_URL = 'https://movie.douban.com/top250'
 
-
+'''
+使用codecs是为了方便处理中文编码
+使用请求中带入header 是为了更加真实模拟浏览器请求，同时避开最基本的反爬虫检验（U-A校验）
+'''
 def download_page(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
