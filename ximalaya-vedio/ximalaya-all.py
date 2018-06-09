85fb5ebd5e8ba32a2e311d24ea401b375048be08
GuoGuiRong <3095764372@qq.com>
2018-06-09 17:59:00 +0800

    重构解析流程

--- ximalaya-vedio/ximalaya-all.py
+++ ximalaya-vedio/ximalaya-all.py
@@ -1,61 +1,13 @@
-﻿import requests
+﻿# encoding=utf-8
+import requests
 from bs4 import BeautifulSoup
 import math
-import sys  
-import time
+import sys
+from multiprocessing import Process
+import os
 
-FIRST_URL = 'http://www.ximalaya.com/category/'
-BASE_URL = 'http://www.ximalaya.com'
-HEADER = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
-    'Cookie':'_xmLog=xm_1528372350671_ji4hfpanjstquv; trackType=web; x_xmly_traffic=utm_source%3A%26utm_medium%3A%26utm_campaign%3A%26utm_content%3A%26utm_term%3A%26utm_from%3A; Hm_lvt_4a7d8ec50cfd6af753c4f8aee3425070=1528372334,1528372351; _ga=GA1.2.105022450.1528372351; Hm_lpvt_4a7d8ec50cfd6af753c4f8aee3425070=1528372547',
-    'If-None-Match': 'W/"28251-CSZM/tu6xVMcaPus+e+1NjCyXrI',
-    'Upgrade-Insecure-Requests':'1'
-    }
-
-category_list={}
-# 解析获取所有分类
-def parse_category(url):
-    data = requests.get(url, headers=HEADER).content
-    # 使用codecs解决乱码问题
-    data = data.decode('utf-8')
-    soup = BeautifulSoup(data)
-    category_hotword = soup.find('div',attrs={'class': 'e-2880429693 category_hotword'})
-    plates = soup.find('div',attrs={'class': 'e-2880429693 plates'})
-    category_hotword_dict={}
-    category_hotword_plats={}
-    
-   
-   # 解析热门分类 
-    for category_hotword_wrapper in category_hotword.find_all('section',attrs={'class': 'e-2880429693 category_hotword-wrapper'}):
-        hotword = category_hotword_wrapper.find('div',attrs={'class':'e-2880429693 hotword'})
-        link_url = hotword.find('a')['href']
-        link_name = hotword.find('img')['alt']
-        category_hotword_dict[link_name] = BASE_URL+link_url
-     
-
-   # 解析其他分类        
-    for plate in plates.find_all('div',attrs={'class': 'e-2880429693 category_plate'}):
-        anchor = plate.find('div',attrs={'class': 'e-2880429693 anchor'})['id']#获取一级目录名称  
-        body = plate.find('div',attrs={'class': 'e-2880429693 body'})
-        subject_dict = {}
-        
-        for subject_wrapper in body.find_all('section',attrs={'class': 'e-2880429693 subject_wrapper'}):   
-           c2 = subject_wrapper.find('div',attrs={'class':'e-2880429693 subject'}).find('a').find('h2').getText()#获取二级目录名称 
-           subject2_dict={}
-           
-           for item in subject_wrapper.find('div',attrs={'class':'e-2880429693 list'}).find_all('a',attrs={'class':'e-2880429693 item separator'}):
-                subject2_dict[item.getText()]= BASE_URL+item['href'] #获取二级目录名称，url
-                
-           subject_dict[c2] = subject2_dict
-           
-        category_hotword_plats[anchor] =  subject_dict 
-          
-    #print(category_hotword_plats)
-    return category_hotword_dict,category_hotword_plats
-            
-# 解析热门分类内容 
 """
+分析：
 # 获取目录分类
 http://www.ximalaya.com/revision/category/allCategoryInfo
 
@@ -76,14 +28,67 @@ User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 
 
 """ 
+# 伪造allCategoryInfo请求头
+catagory_header = {
+'Cookie':'_xmLog=xm_1528372350671_ji4hfpanjstquv; _ga=GA1.2.105022450.1528372351; Hm_lvt_4a7d8ec50cfd6af753c4f8aee3425070=1528440965,1528442411,1528443437,1528520885; Hm_lpvt_4a7d8ec50cfd6af753c4f8aee3425070=1528520885',
+'Referer':'https://www.ximalaya.com/category/',
+'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+}
+category_url = 'http://www.ximalaya.com/revision/category/allCategoryInfo'
+category_dict = {}
+# 获取目录名称以及link
+def get_all_category():
+    data = requests.get(category_url, headers=catagory_header).json()
+    data = data['data']
+    for categorie_list in data:
+        categorys = categorie_list['categories']
+        for category in categorys:
+            category_dict[category['name']] = category['displayName']           
+    return category_dict
+
+    
+# 获取目录下面所有的album
+categoryPageAlbums_url = 'http://www.ximalaya.com/revision/category/queryCategoryPageAlbums'
+categoryPageAlbums_header = {
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+'Cookie': '_xmLog=xm_1528372350671_ji4hfpanjstquv; _ga=GA1.2.105022450.1528372351; Hm_lvt_4a7d8ec50cfd6af753c4f8aee3425070=1528440965,1528442411,1528443437,1528520885; Hm_lpvt_4a7d8ec50cfd6af753c4f8aee3425070=1528520885'
+}
+def get_category_all_albums(category_name):
+    categoryPageAlbums_header['Referer'] = 'https://www.ximalaya.com/'+category_name+'/'
+    params={}
+    params['category'] = category_name
+    params['subcategory'] = ''
+    params['meta'] = ''
+    params['sort'] = '0'
+    params['page'] = '1'
+    params['perPage'] = '30'
+    data = requests.get(categoryPageAlbums_url, headers=categoryPageAlbums_header,params=params).json()
+    data = data['data']
+    total = data['total']
+    albums = data['albums']
+    albums_dict = {}
+    for album in albums:
+        albums_dict[album['albumId']] = album['title']
+    # category_all_albums_dict[category_name] = albums_dict 
+    pages = math.ceil(total/data['pageSize'])
+    for i in range(1,pages):
+        params['page'] = i+1
+        data = requests.get(categoryPageAlbums_url, headers=categoryPageAlbums_header,params=params).json()
+        data = data['data']
+        albums = data['albums']
+        for album in albums:
+            albums_dict[str(album['albumId'])] = album['title']        
+    return albums_dict
+ 
+#获取音频url
 track_ids=[]
-def get_vedio_url(title,albumId):
+def get_vedio_url(category_name,albumId):
     url = 'http://www.ximalaya.com/revision/album'
     header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
     'Cookie':'_xmLog=xm_1528372350671_ji4hfpanjstquv; trackType=web; x_xmly_traffic=utm_source%3A%26utm_medium%3A%26utm_campaign%3A%26utm_content%3A%26utm_term%3A%26utm_from%3A; Hm_lvt_4a7d8ec50cfd6af753c4f8aee3425070=1528372334,1528372351; _ga=GA1.2.105022450.1528372351; Hm_lpvt_4a7d8ec50cfd6af753c4f8aee3425070=1528378517'
     }
-    header['Referer']='http://www.ximalaya.com/'+title+'/'+albumId+'/'
+    header['Referer']='http://www.ximalaya.com/'+category_name+'/'+albumId+'/'
     params={}
     params['albumId']= albumId
     data = requests.get(url,headers=header,params=params).json()
@@ -102,7 +107,7 @@ def get_vedio_url(title,albumId):
         for track in tracks:
             track_ids.append(track['trackId'])   
             
-    header['Referer']='http://www.ximalaya.com/'+title+'/'+albumId+'/'
+    header['Referer']='http://www.ximalaya.com/'+category_name+'/'+albumId+'/'
     header['Cookie']='_xmLog=xm_1528372350671_ji4hfpanjstquv; _ga=GA1.2.105022450.1528372351; Hm_lvt_4a7d8ec50cfd6af753c4f8aee3425070=1528372334,1528372351,1528422022; Hm_lpvt_4a7d8ec50cfd6af753c4f8aee3425070=1528427460'
     track_url='http://www.ximalaya.com/revision/play/tracks'
     track_result = {}
@@ -115,17 +120,37 @@ def get_vedio_url(title,albumId):
             content = content['data']
             tracksForAudioPlay = content['tracksForAudioPlay']
             tarcks = tracksForAudioPlay[0]
-            track_result[tarcks['trackName']]=tarcks['src']     
+            if(tarcks['src'] is not None and tarcks['trackName'] is not None):
+                track_result[tarcks['trackName']]=tarcks['src']     
         except:
             continue
     return track_result
-    
-
+ 
+ #开进程去处理每个目录的音频
+def process(category,categoryName):
+    #创建一级目录
+    dir = 'C://Users/GuiRunning/Desktop/python-learn/ximalaya-vedio/vedio/'+categoryName+'/'
+    isExists=os.path.exists(dir)   
+    if not isExists:
+        os.makedirs(dir) 
+    albums_dict=get_category_all_albums(category)
+    for albumId,albumName in albums_dict.items():
+        file = dir+albumName+'.txt'
+        try:
+            with open(file, 'a',encoding='utf-8') as fp:
+                for trackName,url in get_vedio_url(category,albumId).items():
+                    fp.write(trackName+'==>'+url+'\n')
+        except:
+            continue
+            
 
 def main():
-    data = get_vedio_url('youshengshu','2854032')
-    print(data)
-    print('总条数：%d' %len(data))
+    all_category = get_all_category()
+    for k,v in all_category.items():
+        process(k,v)
+        p = Process(target=process, args=(v,))
+        p.start()
+        p.join()
 
 if __name__ == '__main__':
     main()
